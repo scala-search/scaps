71e043593d108a8708685bbbed309b2efbe588f3
Luegg <wegmaluk@gmail.com>
2015-04-27 16:36:49 +0200

    use own implementation of observables to abstract value propagations

--- webservice/js/src/main/scala/scaps/webservice/ui/Main.scala
+++ webservice/js/src/main/scala/scaps/webservice/ui/Main.scala
@@ -1,20 +1,16 @@
 package scaps.webservice.ui
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.scalajs.js.Any.fromFunction0
-import scala.scalajs.js.Any.fromFunction1
+import scala.concurrent.duration.DurationInt
 import scala.scalajs.js.annotation.JSExport
-import scala.scalajs.js
+
 import org.scalajs.dom
-import org.scalajs.dom.html
-import autowire._
-import scalatags.JsDom.all._
-import scaps.webapi.IndexStatus
-import scaps.webapi.ScapsApi
-import scalatags.JsDom
 import org.scalajs.dom.ext.AjaxException
-import org.scalajs.dom.raw.PopStateEvent
+import org.scalajs.dom.html
 import org.scalajs.dom.raw.FocusEvent
+
+import autowire._
+import scaps.webapi.ScapsApi
 
 @JSExport
 object Main {
@@ -26,24 +22,30 @@ object Main {
       searchField.select()
     })
 
-    searchField.addEventListener("keyup", debounce(400) {
-      event: dom.KeyboardEvent =>
-        handleQuery(searchField.value, container)
-    })
-  }
+    val query = {
+      val q = Variable(searchField.value)
 
-  def debounce[A](ms: Int)(f: A => Unit): A => Unit = {
-    var handle: Int = 0
-    (a: A) => {
-      dom.clearTimeout(handle)
-      handle = dom.setTimeout(() => {
-        f(a)
-      }, ms)
+      searchField.addEventListener("keyup", {
+        (_: dom.KeyboardEvent) =>
+          q() = searchField.value.trim()
+      })
+
+      Observable.debounce(400.millis)(q)
+    }
+
+    val content = Observable.async {
+      query.map(fetchContent)
+    }
+
+    content.foreach { content =>
+      container.innerHTML = ""
+      container.appendChild(content.render)
+      ()
     }
   }
 
-  def handleQuery(query: String, container: html.Div) = {
-    val content = if (query.isEmpty()) {
+  def fetchContent(query: String) = {
+    if (query.isEmpty()) {
       scaps.getStatus().call().map(DomPages.main(_))
     } else {
       scaps.search(query).call().map {
@@ -51,14 +53,7 @@ object Main {
         case Right(results) => DomPages.results(results)
       }
     }.recover {
-      case AjaxException(_) => DomPages.error("The server is currently unreachable. Please try again later.")
+      case AjaxException(_) => DomPages.error("The Scaps service is currently unreachable. Please try again later.")
     }
-
-    content.foreach(replaceContent(container, _))
-  }
-
-  def replaceContent(container: html.Div, content: scalatags.generic.TypedTag[dom.Element, dom.Element, dom.Node]) = {
-    container.innerHTML = ""
-    container.appendChild(content.render)
   }
 }
---
+++ webservice/js/src/main/scala/scaps/webservice/ui/Observable.scala
@@ -0,0 +1,113 @@
+package scaps.webservice.ui
+
+import scala.annotation.implicitNotFound
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+
+import org.scalajs.dom
+
+/**
+ * A small utility library to encapsulate change propagations inspired by scala.Rx
+ * and similar reactive libraries.
+ *
+ * Note, that this implementation is not suited for multithreaded environments and
+ * is (currently) only designed for constructing static dataflow graphs. Other uses
+ * may lead to memory leaks and unexpected behavior.
+ */
+trait Observable[T] { outer =>
+  private var observers: List[T => Unit] = Nil
+
+  protected def notifyObservers(v: T) = {
+    observers.foreach(_(v))
+  }
+
+  /** Register an observer that is called whenever a new value is propagated */
+  def foreach(handler: T => Unit): Unit = {
+    observers = handler :: observers
+  }
+
+  def map[A](f: T => A): Observable[A] =
+    new Observable[A] {
+      override def foreach(handler: A => Unit) = {
+        outer.foreach(f andThen handler)
+      }
+    }
+}
+
+object Observable {
+  /** Creates an observable that fires when a future in `o` is completed. */
+  def async[T](o: Observable[Future[T]])(implicit ec: ExecutionContext): Observable[T] =
+    new Observable[T] {
+      override def foreach(handler: T => Unit) = {
+        o.foreach { f =>
+          f.foreach(handler)
+        }
+      }
+    }
+
+  /**
+   * Creates a new Observable that will not be updated more than once in the
+   *  time period `duration`
+   */
+  def debounce[T](duration: Duration)(o: Observable[T]): Observable[T] =
+    new Observable[T] {
+      override def foreach(handler: T => Unit) = {
+        var handle = 0
+        o.foreach { (v: T) =>
+          dom.clearTimeout(handle)
+          handle = dom.setTimeout(() => handler(v), duration.toMillis)
+        }
+      }
+    }
+
+  def join[A, B](oa: Observable[A], ob: Observable[B]): Observable[(A, B)] =
+    new Observable[(A, B)] {
+      override def foreach(handler: ((A, B)) => Unit) = {
+        var a: Option[A] = None
+        var b: Option[B] = None
+        oa.foreach { va =>
+          a = Some(va)
+          for {
+            vb <- b
+          } {
+            handler((va, vb))
+          }
+        }
+        ob.foreach { vb =>
+          b = Some(vb)
+          for {
+            va <- a
+          } {
+            handler((va, vb))
+          }
+        }
+      }
+    }
+
+  def joinWith[A, B, C](oa: Observable[A], ob: Observable[B])(f: (A, B) => C): Observable[C] =
+    join(oa, ob).map {
+      case (a, b) => f(a, b)
+    }
+}
+
+/**
+ * An observable that can be updated.
+ *
+ * A Variable fires the first time when it is set to a value that is not equal
+ * to the initial value `init`.
+ */
+class Variable[T](init: T) extends Observable[T] {
+  private var v: T = init
+
+  def update(newV: T) = {
+    if (v != newV) {
+      v = newV
+      notifyObservers(newV)
+    }
+  }
+}
+
+object Variable {
+  def apply[T](init: T) = new Variable[T](init)
+}
