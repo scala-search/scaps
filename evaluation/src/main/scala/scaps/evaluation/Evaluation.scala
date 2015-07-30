4ca292ec17cfdef007c97f9bc36d0b16d0fdc8c0
Luegg <wegmaluk@gmail.com>
2015-07-30 11:25:21 +0200

    additional test runs

--- evaluation/src/main/scala/scaps/evaluation/Evaluation.scala
+++ evaluation/src/main/scala/scaps/evaluation/Evaluation.scala
@@ -32,36 +32,58 @@ object Evaluation extends App {
 
   // (name, number of configurations tested, configuration generator)
   val runs: List[(String, Int, Rng[Settings])] = List(
-    ("baseline", 100, randomize(
+    ("baseline", 50, randomize(
       baseSettings,
       baseRngs ++ Map(
         nameBoost -> Rng.oneof(0.1),
         docBoost -> Rng.choosedouble(0, 0.5)))),
-    ("pure FEM", 100, randomize(
+    ("pure FEM", 50, randomize(
       baseSettings.modQuery(_.copy(
         views = true)),
       baseRngs)),
-    ("FEM + Distance", 100, randomize(
+    ("FEM + Distance", 50, randomize(
       baseSettings.modQuery(_.copy(
         views = true)),
       baseRngs ++ Map(
         distanceBoostWeight -> Rng.choosedouble(0, 2)))),
-    ("FEM + Depth", 100, randomize(
+    ("FEM + Depth", 50, randomize(
       baseSettings.modQuery(_.copy(
         views = true)),
       baseRngs ++ Map(
         depthBoostWeight -> Rng.choosedouble(0, 2)))),
-    ("FEM + Type Frequency", 100, randomize(
+    ("FEM + Type Frequency", 50, randomize(
       baseSettings.modQuery(_.copy(
         views = true)),
       baseRngs ++ Map(
         typeFrequencyWeight -> Rng.choosedouble(0, 2)))),
-    ("FEM + Fractions", 100, randomize(
+    ("FEM + Fractions", 50, randomize(
       baseSettings.modQuery(_.copy(
         views = true,
         fractions = true)),
       baseRngs)),
-    ("All Extensions", 1000, randomize(
+    ("FEM + Distance + Type Frequencies", 500, randomize(
+      baseSettings.modQuery(_.copy(
+        views = true,
+        fractions = false)),
+      baseRngs ++ Map(
+        distanceBoostWeight -> Rng.choosedouble(0, 2),
+        typeFrequencyWeight -> Rng.choosedouble(0, 2)))),
+    ("FEM + Distance + Type Frequencies + Depth", 500, randomize(
+      baseSettings.modQuery(_.copy(
+        views = true,
+        fractions = false)),
+      baseRngs ++ Map(
+        distanceBoostWeight -> Rng.choosedouble(0, 2),
+        depthBoostWeight -> Rng.choosedouble(0, 2),
+        typeFrequencyWeight -> Rng.choosedouble(0, 2)))),
+    ("FEM + Distance + Type Frequencies + Fractions", 500, randomize(
+      baseSettings.modQuery(_.copy(
+        views = true,
+        fractions = true)),
+      baseRngs ++ Map(
+        distanceBoostWeight -> Rng.choosedouble(0, 2),
+        typeFrequencyWeight -> Rng.choosedouble(0, 2)))),
+    ("All Extensions", 500, randomize(
       baseSettings.modQuery(_.copy(
         views = true,
         fractions = true)),
@@ -73,62 +95,62 @@ object Evaluation extends App {
   var engine = Common.initSearchEngine(baseSettings, evaluationSettings)
 
   using(new FileWriter(outputFile)) { writer =>
-    using(new FileWriter(statsOutputFile)) { statsWriter =>
-      val headers = List(
-        "run",
-        QuerySettings.views,
-        QuerySettings.fractions,
-        QuerySettings.lengthNormWeight,
-        QuerySettings.depthBoostWeight,
-        QuerySettings.distanceBoostWeight,
-        QuerySettings.typeFrequencyWeight,
-        QuerySettings.nameBoost,
-        QuerySettings.docBoost,
-        QuerySettings.fingerprintFrequencyCutoff,
-        "MAP",
-        "R@10",
-        "t")
+    val headers = List(
+      "run",
+      QuerySettings.views,
+      QuerySettings.fractions,
+      QuerySettings.lengthNormWeight,
+      QuerySettings.depthBoostWeight,
+      QuerySettings.distanceBoostWeight,
+      QuerySettings.typeFrequencyWeight,
+      QuerySettings.nameBoost,
+      QuerySettings.docBoost,
+      QuerySettings.fingerprintFrequencyCutoff,
+      "MAP",
+      "R@10",
+      "t")
 
-      println(headers.mkString("", "; ", ";\n"))
-      writer.write(headers.mkString("", "; ", ";\n"))
+    println(headers.mkString("", "; ", ";\n"))
+    writer.write(headers.mkString("", "; ", ";\n"))
 
-      runs.foreach {
-        case (runName, noConfigurations, settingsGenerator) =>
-          println(s"start '$runName'")
-          val allStats = settingsGenerator.fill(noConfigurations).runUnsafe(Math.pow(42, 42).toLong).map { settings =>
-            engine = Common.updateSearchEngine(engine, settings)
-            Common.runQueries(engine, evaluationSettings.queries).fold(
-              errors => {
-                println(errors)
-                ???
-              },
-              stats => {
-                val cells = List[Any](
-                  runName,
-                  settings.query.views,
-                  settings.query.fractions,
-                  settings.query.lengthNormWeight,
-                  settings.query.depthBoostWeight,
-                  settings.query.distanceBoostWeight,
-                  settings.query.typeFrequencyWeight,
-                  settings.query.nameBoost,
-                  settings.query.docBoost,
-                  settings.query.fingerprintFrequencyCutoff,
-                  stats.meanAveragePrecision,
-                  stats.meanRecallAt10,
-                  stats.duration.toMillis + " ms")
+    runs.foreach {
+      case (runName, noConfigurations, settingsGenerator) =>
+        println(s"start '$runName'")
+        val allStats = settingsGenerator.fill(noConfigurations).runUnsafe(Math.pow(42, 42).toLong).map { settings =>
+          engine = Common.updateSearchEngine(engine, settings)
+          Common.runQueries(engine, evaluationSettings.queries).fold(
+            errors => {
+              println(errors)
+              ???
+            },
+            stats => {
+              val cells = List[Any](
+                runName,
+                settings.query.views,
+                settings.query.fractions,
+                settings.query.lengthNormWeight,
+                settings.query.depthBoostWeight,
+                settings.query.distanceBoostWeight,
+                settings.query.typeFrequencyWeight,
+                settings.query.nameBoost,
+                settings.query.docBoost,
+                settings.query.fingerprintFrequencyCutoff,
+                stats.meanAveragePrecision,
+                stats.meanRecallAt10,
+                stats.duration.toMillis + " ms")
 
-                println(cells.mkString("", "; ", ";\n"))
-                writer.write(cells.mkString("", "; ", ";\n"))
+              println(cells.mkString("", "; ", ";\n"))
+              writer.write(cells.mkString("", "; ", ";\n"))
 
-                (stats, cells)
-              })
-          }
+              (stats, cells)
+            })
+        }
 
+        using(new FileWriter(statsOutputFile)) { statsWriter =>
           val run = RunStats(runName, allStats)
           println(run)
           statsWriter.write(run.toString)
-      }
+        }
     }
   }.get
 
